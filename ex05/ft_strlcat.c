69ff74da3080ecdfe6a43914f20512a56ddfdd02
Onur Boran Dalkilinc <odalkili@k1m06s08.42istanbul.com.tr>
2024-02-08 01:48:39 +0300

    a

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -1,25 +1,38 @@
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odalkili <odalkili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 01:47:37 by odalkili          #+#    #+#             */
+/*   Updated: 2024/02/08 01:47:38 by odalkili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	j;
-	unsigned int	k;
+	unsigned int	res;
 
-	k = 0;
 	i = 0;
 	j = 0;
+	res = 0;
 	while (dest[i] != '\0')
 		i++;
-	while (src[j] != '\0' && i < (size - 1))
+	while (src[res] != '\0')
+		res++;
+	if (size <= i)
+		res += size;
+	else
+		res += i;
+	while (src[j] != '\0' && (i + 1) < size)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 	dest[i] = '\0';
-	while (src[j] != '\0')
-	{
-		j++;
-		k++;
-	}
-	return (i + k);
+	return (res);
 }
