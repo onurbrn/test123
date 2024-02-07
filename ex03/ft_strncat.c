6f77e410ba637cd7a27b1d2ad9da7f938107490a
Onur Boran Dalkilinc <odalkili@k1m06s08.42istanbul.com.tr>
2024-02-08 02:44:35 +0300

    a

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    ft_strncat.c                                      :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: odalkili <odalkili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 01:47:42 by odalkili          #+#    #+#             */
-/*   Updated: 2024/02/08 01:55:03 by odalkili         ###   ########.fr       */
+/*   Updated: 2024/02/08 02:44:16 by odalkili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strncat(char *dest, char *src, unsigned int nb)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	unsigned int	j;
 	unsigned int	i;
+	unsigned int	j;
 
-	j = 0;
 	i = 0;
+	j = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 	while (src[j] != '\0' && j < nb)
 	{
 		dest[i] = src[j];
