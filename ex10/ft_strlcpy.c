d87f1984d624480c6dfb8e2e1bd738f41a604621
Onur Boran Dalkilinc <odalkili@k1m06s08.42istanbul.com.tr>
2024-02-08 02:37:34 +0300

    a

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,24 +6,35 @@
 /*   By: odalkili <odalkili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 17:13:47 by odalkili          #+#    #+#             */
-/*   Updated: 2024/02/06 20:17:57 by odalkili         ###   ########.fr       */
+/*   Updated: 2024/02/08 02:37:03 by odalkili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int counter;
+
+	counter = 0;
+	while (*str != '\0')
+	{
+		counter++;
+		str++;
+	}
+	return (counter);
+}
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	count;
 
-	count = 0;
+	if (size == 0)
+		return (ft_strlen(src));
 	i = 0;
 	while (i < size - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (dest[count] != '\0')
-		count++;
 	dest[i] = '\0';
-	return (count);
+	return (ft_strlen(src));
 }
